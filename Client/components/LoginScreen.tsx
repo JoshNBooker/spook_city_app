0adab237eb7d45c8bd71b6dc5d4cb6c204e8a443
Blair Scott <blairs4932@outlook.com>
2023-10-10 11:18:17 +0100

    added LoginPage

--- Client/components/LoginScreen.tsx
+++ Client/components/LoginScreen.tsx
@@ -1,12 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground, TextInput } from 'react-native'
 import React from 'react'
 
-export default function LoginScreen() {
+import { useState } from 'react';
+
+const LoginScreen = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (text: string) => {
+    setUsername(text);
+  }
+
+  const handlePasswordChange = (text: string) => {
+    setPassword(text);
+  }
+
+  const handleLogin = () => {
+    if (username === 'admin' && password === 'password') {
+      alert('Login successful');
+      } else {
+      alert('Username or password is incorrect');
+      }
+  }
+
   return (
-    <View>
-      <Text>This is a Login Screen</Text>
+    <View style={styles.container}>
+
+        <View style={styles.loginContainer}>
+          
+          <Text style={styles.title}>Login</Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            onChangeText={handleUsernameChange}
+            value={username}
+          />
+
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            onChangeText={handlePasswordChange}
+            value={password}
+            secureTextEntry={true}
+          />
+
+          <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  loginContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: 20,
+    borderRadius: 10,
+    margin: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  loginButton: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input: {
+    backgroundColor: '#FFFFFF',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    border: 0,
+  }
+})}
