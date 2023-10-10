504cbb9a1f51e4d5b92bd654b3b39a73b4813cfe
Josh Booker <josh.booker2@gmail.com>
2023-10-10 13:44:37 +0100

    added some styling and conditional rendering

--- Client/ItemFull.tsx
+++ Client/ItemFull.tsx
@@ -4,14 +4,16 @@ import { User, Ghost } from './types/Types';
 import Swiper from 'react-native-swiper';
 
 interface ItemFullProps {
-	show?: boolean;
-	hide?: boolean;
+	hidden: boolean;
 	users: User[];
-	ghosts: Ghost[];
+	ghosts?: Ghost[];
 }
 
-export default function ItemFull({ show, hide, users, ghosts }: ItemFullProps) {
+export default function ItemFull({ hidden, users, ghosts }: ItemFullProps) {
+	console.log('hidden:', hidden);
+
 	const firstUser = users[0];
+
 	if (!users || users.length === 0) {
 		return (
 			<View>
@@ -19,38 +21,44 @@ export default function ItemFull({ show, hide, users, ghosts }: ItemFullProps) {
 			</View>
 		);
 	}
+
 	return (
-		<View>
-			<Text>{firstUser.userName}</Text>
-			<Text>Rank: {firstUser.rank}</Text>
-			<Text>Score: {firstUser.points}</Text>
-			<View style={styles.overlay}>
-				<Swiper>
-					<View style={styles.ghostContainer}>
-						{ghosts.map((ghost, index) => {
-							return (
-								<View style={styles.ghostTile} key={index}>
-									<Text style={styles.tileText}>{ghost.name}</Text>
-								</View>
-							)
-						})}
-					</View>
-					<View></View>
-				</Swiper>
-			</View>
+		<View style={!hidden ? styles.centeredContent : undefined}>
+			{hidden ? (
+				<View>
+					<Text>{firstUser.userName}</Text>
+					<Text>Rank: {firstUser.rank}</Text>
+					<Text>Score: {firstUser.points}</Text>
+				</View>
+			) : (
+				<View style={styles.overlay}>
+					<Swiper>
+						<View style={styles.ghostContainer}>
+							{ghosts.map((ghost, index) => {
+								return (
+									<View style={styles.ghostTile} key={index}>
+										<Text style={styles.tileText}>
+											{ghost.name}
+										</Text>
+									</View>
+								);
+							})}
+						</View>
+						<View></View>
+					</Swiper>
+				</View>
+			)}
 		</View>
-		
 	);
 }
 
 const styles = StyleSheet.create({
 	overlay: {
-		position: 'absolute',
 		backgroundColor: '#e1e1fc',
-		opacity: 0.5,
-		height: "80%",
-		width: "80%",
-		zIndex: 1,
+		opacity: 0.9,
+		height: '90%',
+		width: '90%',
+		zIndex: 5,
 	},
 	ghostTile: {
 		backgroundColor: '#0000FF',
@@ -60,7 +68,7 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		height: 'auto',
 		width: '30%',
-		zIndex: 2,
+		zIndex: 4,
 	},
 	ghostContainer: {
 		display: 'flex',
@@ -70,8 +78,15 @@ const styles = StyleSheet.create({
 		height: '100%',
 		width: '100%',
 		gap: 3,
+		zIndex: 2,
 	},
 	tileText: {
 		color: '#FBF7F5',
 	},
-})
+	centeredContent: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		zIndex: 1,
+	},
+});
