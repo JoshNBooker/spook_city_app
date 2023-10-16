ae90ef8e90a52ba79130ccdec79ba5362568ac1c
Josh Booker <josh.booker2@gmail.com>
2023-10-16 21:11:12 +0100

    created modal for map

--- Client/components/MapComponent.tsx
+++ Client/components/MapComponent.tsx
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE, Camera } from 'react-native-maps';
+import {
+	StyleSheet,
+	View,
+	Text,
+	Pressable,
+	Modal,
+	Alert,
+	Image,
+	Button,
+} from 'react-native';
+import MapView, {
+	Marker,
+	PROVIDER_GOOGLE,
+	Camera,
+	Overlay,
+} from 'react-native-maps';
 import { ImageURISource } from 'react-native';
 import { Location, User, Ghost } from '../types/Types';
 import SwipeUp from './SwipeUp';
 import * as UserLocation from 'expo-location';
 import { LocationObject } from 'expo-location';
 import { Double } from 'react-native/Libraries/Types/CodegenTypes';
+import getGhostImage from '../ghostImages';
 
 interface MapComponentProps {
 	locations: Location[];
@@ -29,11 +44,15 @@ const MapComponent: React.FC<MapComponentProps> = ({
 	ghosts,
 }) => {
 	const [userLocation, setUserLocation] = useState<LocationObject>(null);
-	const [foundGhost, setFoundGhost] = useState<Location>(null);
+	const [foundGhost, setFoundGhost] = useState<Location>(locations[3]);
+	const [foundGhostModalVisible, setFoundGhostModalVisible] =
+		useState<boolean>(false);
 	const [sortedGhostLocations, setSortedGhostLocations] = useState<
 		Location[]
 	>([]);
-
+	const [modalVisible, setModalVisible] = useState(false);
+	const [selectedGhostMapScreen, setSelectedGhostMapScreen] =
+		useState<Ghost>(null);
 	const compareLocations = async () => {
 		try {
 			await updateUserLocation();
@@ -138,6 +157,20 @@ const MapComponent: React.FC<MapComponentProps> = ({
 	console.log('ghosts[0]', ghosts[0]);
 	console.log('locations[0].ghost: ', locations[0].ghost);
 
+	const handleMarkerClick = (location: Location) => {
+		setModalVisible(!modalVisible);
+		setSelectedGhostMapScreen(location.ghost);
+		console.log('modal visible: ', modalVisible);
+		console.log(`marker hit at ${location.name}`);
+	};
+	const handleFoundMarkerClick = (location: Location) => {
+		setFoundGhostModalVisible(!foundGhostModalVisible);
+		console.log('found modal visible: ', foundGhostModalVisible);
+	};
+
+	console.log('normal modal visible? :', modalVisible);
+	console.log('found modal visible? :', foundGhostModalVisible);
+
 	return (
 		<View>
 			<MapView
@@ -156,10 +189,22 @@ const MapComponent: React.FC<MapComponentProps> = ({
 				showsBuildings={true}
 			>
 				{locations.map((location, index) => {
+					let opacity = 1 - index * 0.1;
 					const imagePath = location.ghost.discovered
 						? require('../assets/foundGhost.png')
 						: require('../assets/AnimatedGhost1.gif');
-					return (
+					return location.ghost == foundGhost.ghost ? (
+						<Marker
+							coordinate={{
+								latitude: location.coordinateX,
+								longitude: location.coordinateY,
+							}}
+							key={index}
+							image={require('../assets/atGhost.png')}
+							style={{ opacity: opacity }}
+							onPress={() => handleFoundMarkerClick(location)}
+						/>
+					) : (
 						<Marker
 							coordinate={{
 								latitude: location.coordinateX,
@@ -167,10 +212,104 @@ const MapComponent: React.FC<MapComponentProps> = ({
 							}}
 							key={index}
 							image={imagePath}
+							onPress={() => handleMarkerClick(location)}
+							style={{ opacity: opacity }}
 						/>
 					);
 				})}
 			</MapView>
+			{modalVisible && !foundGhostModalVisible && (
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={modalVisible}
+					onRequestClose={() => {
+						Alert.alert('Modal has been closed.');
+						setModalVisible(!modalVisible);
+					}}
+				>
+					<View style={styles.centered}>
+						<View style={styles.modalView}>
+							<View style={styles.selectedGhostContainer}>
+								<Text style={styles.ghostName}>
+									{selectedGhostMapScreen.discovered
+										? selectedGhostMapScreen.name
+										: 'Unknown Ghost'}
+								</Text>
+								<Image
+									source={
+										selectedGhostMapScreen.discovered
+											? getGhostImage(
+													selectedGhostMapScreen
+											  )
+											: require('../images/GhostPictures/undiscoveredGhost.jpg')
+									}
+									style={styles.ghostImage}
+								/>
+								<Text style={styles.ghostDescription}>
+									{!selectedGhostMapScreen.discovered
+										? `${selectedGhostMapScreen.hiddenDescription}. You have not encountered this spirit yet`
+										: selectedGhostMapScreen.description}
+								</Text>
+							</View>
+							<Button
+								title="close"
+								onPress={() => setModalVisible(false)}
+							/>
+						</View>
+					</View>
+				</Modal>
+			)}
+			{!modalVisible && foundGhostModalVisible && (
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={foundGhostModalVisible}
+					onRequestClose={() => {
+						Alert.alert('Modal has been closed.');
+						setFoundGhostModalVisible(!foundGhostModalVisible);
+					}}
+				>
+					<View style={styles.centered}>
+						<View style={styles.modalView}>
+							<Text style={styles.modalText}>
+								Ghost Encounter!
+							</Text>
+							<Image
+								source={getGhostImage(foundGhost.ghost)}
+								style={styles.foundGhostImage}
+							/>
+							<Pressable
+								style={[
+									styles.modalButton,
+									styles.modalButtonClose,
+								]}
+								onPress={() =>
+									setFoundGhostModalVisible(
+										!foundGhostModalVisible
+									)
+								}
+							>
+								<Text style={styles.modalText}>
+									You have found {foundGhost.ghost.name} in{' '}
+									{foundGhost.name} - {foundGhost.description}
+									. Dare you summon this spectre?
+								</Text>
+								<View style={styles.modalButtonContainer}>
+									<Button title="Yes"></Button>
+									<Button
+										title="No"
+										onPress={() => {
+											setFoundGhostModalVisible(false);
+											setModalVisible(false);
+										}}
+									></Button>
+								</View>
+							</Pressable>
+						</View>
+					</View>
+				</Modal>
+			)}
 			<SwipeUp users={users} ghosts={ghosts} />
 		</View>
 	);
@@ -185,6 +324,72 @@ const styles = StyleSheet.create({
 		width: '100%',
 		backgroundColor: '#0000FF',
 		zIndex: 1,
+	},
+	foundMarker: {
+		shadowColor: 'rgba(0, 0, 0, 0.5)',
+		shadowOffset: { width: 0, height: 4 },
+		shadowRadius: 5,
+		shadowOpacity: 1,
+	},
+	centered: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	modalView: {
+		margin: 20,
+		backgroundColor: 'rgba(26, 26, 26, 0.97)',
+		borderRadius: 30,
+		padding: 20,
+	},
+	selectedGhostContainer: {
+		paddingVertical: 20,
+		paddingHorizontal: 10,
+		borderRadius: 10,
+		alignItems: 'center',
+		justifyContent: 'center',
+		elevation: 10,
+		shadowColor: '#fff',
+		shadowOffset: { width: 0, height: 5 },
+		shadowOpacity: 0.3,
+		shadowRadius: 10,
+	},
+	ghostName: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		marginBottom: 20,
+		color: '#FBF7F5',
+	},
+	ghostImage: {
+		height: 500,
+		width: 300,
+	},
+	ghostDescription: {
+		fontSize: 14,
+		color: '#FBF7F5',
+		marginTop: '5%',
+	},
+	modalText: {
+		fontSize: 30,
+		fontWeight: 'bold',
+		color: '#FBF7F5',
+	},
+	modalButton: {
+		margin: 10,
+		padding: 10,
+	},
+	modalButtonClose: {
+		backgroundColor: '#da6512',
+		borderRadius: 10,
+		padding: 10,
+	},
+	modalButtonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	foundGhostImage: {
+		height: 300,
+		width: 200,
 	},
 });
 
